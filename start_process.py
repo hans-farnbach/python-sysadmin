b114550042e3f1d9e1a56e2e4780066248729e3f
hans-farnbach <hans@farnbach.net>
2018-03-09 21:56:11 -0700

    Commented

--- start_process.py
+++ start_process.py
@@ -1,4 +1,5 @@
-from os import execlp                   # Calling child process
+# Given the name of a process, will execute process
+from os import execlp           # Calling child process
 
-process = input("Process: ")        # Ask for a process
-execlp(process, "python")       # Run it, putting something as a 2nd arg so the OS knows python invoked this process
+process = input("Process: ")    # Ask for a process name to run
+execlp(process, "python")       # Run it, putting something as a 2nd arg so the OS knows Python invoked this process
