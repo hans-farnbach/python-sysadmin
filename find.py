7dba9a123a615d42312f8906de784d3f58b3d97e
hans-farnbach <hans@farnbach.net>
2018-03-09 21:47:14 -0700

    Comment it

--- find.py
+++ find.py
@@ -1,6 +1,8 @@
+# Asks for a filename and tells you where it is
+
 from os import walk, path
 
-f = input("Find: ")                 # Ask for file to find; EXACT
+f = input("Find: ")                     # Ask for file to find; EXACT
 for root, dirs, files in walk("/"):     # Go through everything
-  if f in files:                        # If filename found
-    print(path.join(root,f))            # Print it out, adding directory to filename
+  if f in files:                        # If filename found....
+    print(path.join(root,f))            #   print it out, adding directory to filename
