7e2b4e396a70e953ccbf03aefbb0ddd4db671300
hans-farnbach <hans@farnbach.net>
2018-03-09 21:54:07 -0700

    Commented

--- kill_process.py
+++ kill_process.py
@@ -1,12 +1,14 @@
+# Given a process name, kills or suspends the process
 import psutil                           # pip install psutil
 
-proc = input("Process: ")           # Ask for a process to kill
-ks = input("Kill (y/n)?")           # Ask to kill, otherwise will suspend
+proc = input("Process: ")               # Ask for the name of a process to kill
+ks = input("Kill (y/n)?")               # Ask to kill, otherwise will suspend
 
-if ks[0] == "y" or ks[0] == "Y":        # If we should kill
-  for p in psutil.process_iter():       # Go through list
+if ks[0] == "y" or ks[0] == "Y":        # If we should kill; looks at only first character so that user can type "y" or "yes"
+  for p in psutil.process_iter():       # Go through process list
     if p.name() == proc:                # Find it
       p.kill()                          # Kill it
+
 else:                                   # No kill
   for p in psutil.process_iter():       # Go through
     if p.name() == proc:                # Find it
